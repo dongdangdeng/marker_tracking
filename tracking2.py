0d14ad767ac8b19175c7c7369644c86644246b9f
k-matsunawa-shinwa <k-matsunawa@shinwa-co.co,jp>
2022-12-01 16:18:13 +0900

    同じidのマーカーが複数検知された際のエラー修正

--- tracking2.py
+++ tracking2.py
@@ -8,9 +8,9 @@ import tqdm
 
 random.seed(0)
 
-IS_DEBUG = True
+IS_DEBUG = False
 
-input_video_path = "src/mov/test06.mp4"
+input_video_path = "src/mov/VibrationTest/oricon_g_hd_240.mp4"
 cap = cv2.VideoCapture(input_video_path)
 fps = int(cap.get(cv2.CAP_PROP_FPS))    # 動画のfps
 # current_frame = 1   # 現在の動画フレーム
@@ -46,7 +46,7 @@ output_path = "output/test/markers.csv"
 print("parsing markers...")
 for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
     IS_DEBUG and print("frame: " + str(current_frame))
-    if current_frame == 100:
+    if IS_DEBUG and current_frame == 10:
         print("stop")
     ret, img = cap.read()
     if not ret:
@@ -54,6 +54,41 @@ for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
 
     # マーカー認識
     corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
+
+    # if current_frame == 1175:
+    #     from IPython.core.debugger import Pdb; Pdb().set_trace()
+
+    # 同じidのマーカーが複数検知された際の処理（前回検知されたmarkerとの距離が近い方のみを残す）
+    # TODO 一応動いているがデータあっているか検証
+    if ids is not None:
+        id_list = list(map(lambda id: id[0], ids))   # idsの値をそのまま平のリストに変換
+    duplicate_ids = [x for x in set(
+        id_list) if id_list.count(x) > 1]   # 重複しているidがあれば抜き出し
+    if len(duplicate_ids) > 0:
+        for id in duplicate_ids:
+            IS_DEBUG and print("duplicate id: " + str(id))
+            cpxs = markers_hist[id]['cpx'].dropna().dropna()
+            cpys = markers_hist[id]['cpy'].dropna().dropna()
+            if len(cpxs) != 0 and len(cpys) != 0:   # 座標にNaNしかない場合（初めてW誤検知した場合等）は処理しない
+                last_cpx = list(cpxs[-1:])[0]
+                last_cpy = list(cpys[-1:])[0]
+
+                corners_indexes_from_duplicate_id = [i for i, x in enumerate(
+                    ids) if x == id]  # 重複idのデータが入っているcornersのindexリスト
+                IS_DEBUG and print("duplicate_indexes: ",
+                                   corners_indexes_from_duplicate_id)
+                # (last_cpx, last_cpy)と今検知された座標の距離のリスト（index=該当のcornersのindex）
+                distances = pd.Series(dtype=float)
+                for corner_id in corners_indexes_from_duplicate_id:
+                    # cpx座標とcpy座標の距離
+                    distance_x, distance_y = np.average(
+                        corners[corner_id][0], axis=0).astype(int) - [last_cpx, last_cpy]
+                    distances.at[corner_id] = np.sqrt(
+                        distance_x ** 2 + distance_y ** 2)    # 各xy座標の距離から2点間の距離に変換し、リストに追加
+                drop_indexes = distances.index.drop(
+                    labels=[distances.idxmin()]).tolist()
+                corners = np.delete(corners, drop_indexes, axis=0)
+                ids = np.delete(ids, drop_indexes, axis=0)
 
     markers_current = pd.DataFrame()
     markers_tmp = []
@@ -85,8 +120,7 @@ for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
                 6]  # [deg]
 
             # 中心座標(ピクセル単位)
-            cpx = int((this_corner[0][0] + this_corner[2][0]) / 2)
-            cpy = int((this_corner[0][1] + this_corner[2][1]) / 2)
+            cpx, cpy = np.average(this_corner, axis=0).astype(int)
             # マーカー座標をmarkers(DataFrame)に書き込み
             this_marker = pd.DataFrame([{"x": tvec[0], "y": tvec[1], "z": tvec[2],
                                          "roll": euler_angle[0][0], "pitch": euler_angle[1][0], "yaw": euler_angle[2][0],
@@ -102,14 +136,15 @@ for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
         if len(markers_tmp) < 2:    # マーカーが1つのみ検出された場合
             # concatで複数のマーカーDFを結合してマルチカラムを作成しているため、マーカーDFが1つだけの場合
             # 1つのマーカーを複製して無理やりconcatして、複製した方を最後に削除している
-            markers_tmp = [markers_tmp[0], markers_tmp[0]]
-            markers_current = pd.concat(markers_tmp, axis=1, keys=[
+            markers_current = pd.concat([markers_tmp[0], markers_tmp[0]], axis=1, keys=[
                                         ids[0][0], DELETE_KEY])  # 後で消す方のkey=DELETE_KEY
+
         else:
             markers_current = pd.concat(
                 markers_tmp, axis=1, keys=np.squeeze(ids))
 
-        markers_hist = pd.concat(   # TODO markers_currentの値がNaNしかない場合、markers_tmpの内容が何であれNaNになってしまう？
+        IS_DEBUG and print(markers_current)
+        markers_hist = pd.concat(
             [markers_hist, markers_current], ignore_index=True)
     else:  # マーカーが1つも検出できなかった場合
         markers_hist.loc[current_frame] = np.nan
@@ -131,5 +166,5 @@ for col in tqdm.tqdm(markers_hist.columns):
             markers_hist[col] = markers_hist[col].astype(int)
 
 markers_hist
-# markers_hist.to_csv(output_path)
-# print("out put parsed markers '" + str(output_path) + "'")
+markers_hist.to_csv(output_path)
+print("out put parsed markers '" + str(output_path) + "'")
