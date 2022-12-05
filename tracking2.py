7b418d6abcdbea5a6875124c802dfcbbd7920efb
k-matsunawa-shinwa <k-matsunawa@shinwa-co.co,jp>
2022-12-05 14:18:52 +0900

    鮮鋭化フィルタの実装

--- tracking2.py
+++ tracking2.py
@@ -9,10 +9,13 @@ import sys
 
 random.seed(0)
 
-IS_DEBUG = False
+IS_DEBUG = False                      # デバッグモード
+IS_COMPLEMENT_MISSING_VALUES = False  # 欠損値の補完を行うか
+IS_APPLY_FILTERS = True               # 鮮鋭化フィルタを適用するか
 
 # 解析する動画のパス
-input_video_path = "src/mov/VibrationTest/oricon_g_hd_240.mp4"
+input_video_path = "src/mov/VibrationTest/t_hd_60.mp4"
+
 print("loading '" + input_video_path + "'")
 cap = cv2.VideoCapture(input_video_path)
 if not (cap.isOpened()):
@@ -49,6 +52,17 @@ orbit_length = 100
 # マーカーの履歴（初期状態でマルチカラムのDFをconcatしてもエラーにならないように最低限フォーマットを整える）
 markers_hist = pd.DataFrame([np.nan], columns=[[DELETE_KEY], [0]])
 
+# 鮮鋭化フィルタ
+def applyFilters(img):
+    k = 2
+    sharp_kernel = np.array([
+        [-k / 9, -k / 9, -k / 9],
+        [-k / 9, 1 + 8 * k / 9, k / 9],
+        [-k / 9, -k / 9, -k / 9]
+    ], np.float32)
+    img_bltrl = cv2.bilateralFilter(src=img, d=10, sigmaColor=75, sigmaSpace=75)
+    # img_sp = cv2.filter2D(img_bltrl, -1, sharp_kernel).astype("uint8")
+    return img_bltrl
 
 print("parsing markers...")
 for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
@@ -62,47 +76,61 @@ for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
     # マーカー認識
     corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
 
-    # if current_frame == 1175:
-    #     from IPython.core.debugger import Pdb; Pdb().set_trace()
-
     # 同じidのマーカーが複数検知された際の処理（前回検知されたmarkerとの距離が近い方のみを残す）
     # TODO 一応動いているがデータあっているか検証
-    if ids is not None:
-        id_list = list(map(lambda id: id[0], ids))   # idsの値をそのまま平のリストに変換
-    duplicate_ids = [x for x in set(
-        id_list) if id_list.count(x) > 1]   # 重複しているidがあれば抜き出し
+    if ids is not None :
+        id_list = list(map(lambda id : id[0] , ids))   # idsの値をそのまま平のリストに変換
+        duplicate_ids = [x for x in set(id_list) if id_list.count(x) > 1]   # 重複しているidがあれば抜き出し
+    else:
+        id_list = []
+        duplicate_ids = []
+        
     if len(duplicate_ids) > 0:
         for id in duplicate_ids:
-            IS_DEBUG and print("duplicate id: " + str(id))
-            cpxs = markers_hist[id]['cpx'].dropna().dropna()
-            cpys = markers_hist[id]['cpy'].dropna().dropna()
+            IS_DEBUG and print(f"duplicate id: {id}")
+            cpxs = markers_hist[id]['cpx'].dropna()
+            cpys = markers_hist[id]['cpy'].dropna()
             if len(cpxs) != 0 and len(cpys) != 0:   # 座標にNaNしかない場合（初めてW誤検知した場合等）は処理しない
                 last_cpx = list(cpxs[-1:])[0]
                 last_cpy = list(cpys[-1:])[0]
-
-                corners_indexes_from_duplicate_id = [i for i, x in enumerate(
-                    ids) if x == id]  # 重複idのデータが入っているcornersのindexリスト
-                IS_DEBUG and print("duplicate_indexes: ",
-                                   corners_indexes_from_duplicate_id)
-                # (last_cpx, last_cpy)と今検知された座標の距離のリスト（index=該当のcornersのindex）
-                distances = pd.Series(dtype=float)
+                
+                corners_indexes_from_duplicate_id = [i for i, x in enumerate(ids) if x == id] # 重複idのデータが入っているcornersのindexリスト
+                IS_DEBUG and print("duplicate_indexes: ", corners_indexes_from_duplicate_id)
+                distances = pd.Series(dtype=float)  # (last_cpx, last_cpy)と今検知された座標の距離のリスト（index=該当のcornersのindex）
                 for corner_id in corners_indexes_from_duplicate_id:
                     # cpx座標とcpy座標の距離
-                    distance_x, distance_y = np.average(
-                        corners[corner_id][0], axis=0).astype(int) - [last_cpx, last_cpy]
-                    distances.at[corner_id] = np.sqrt(
-                        distance_x ** 2 + distance_y ** 2)    # 各xy座標の距離から2点間の距離に変換し、リストに追加
-                drop_indexes = distances.index.drop(
-                    labels=[distances.idxmin()]).tolist()
+                    distance_x, distance_y = np.average(corners[corner_id][0], axis=0).astype(int) - [last_cpx, last_cpy]
+                    distances.at[corner_id] = np.sqrt(distance_x ** 2 + distance_y ** 2)    # 各xy座標の距離から2点間の距離に変換し、リストに追加
+                drop_indexes = distances.index.drop(labels=[distances.idxmin()]).tolist()
                 corners = np.delete(corners, drop_indexes, axis=0)
                 ids = np.delete(ids, drop_indexes, axis=0)
+                id_list = list(map(lambda id : id[0] , ids)) # TODO DRY
+
+    # フィルター適用処理
+    # フィルター適用後のimgでのみ検出されたマーカーを追加
+    if IS_APPLY_FILTERS:
+        img_f = applyFilters(img)
+        corners_f, ids_f, rejectedImgPoints_f = aruco.detectMarkers(img_f, dictionary)
+        ids_f_list = list(map(lambda id : id[0] , ids_f))
+        origin_ids = [id for id in ids_f_list if id not in id_list]   # フィルター適用後のみ検出されたid
+        origin_id_indexes = [ids_f_list.index(i) for i in origin_ids]   # origin_idsのidに対応するindex
+        id_list.extend(origin_ids)  # id_listに新しく検出されたidを追加
+        corners_list = list(corners)
+        for i in origin_id_indexes: # cornersに新しく検出された座標を追加
+            corners_list.append(corners_f[i])
+        if IS_DEBUG and len(origin_ids) > 0:
+            print(f'[{current_frame}] detect marker(s) on filterd img : {origin_ids}')
+        
+    ### DEBUG
+    # if len(ids) < len(ids_f):
+    #     from IPython.core.debugger import Pdb; Pdb().set_trace()
 
     markers_current = pd.DataFrame()
     markers_tmp = []
-    if len(corners) > 0:
+    if len(corners_list) > 0:
         # マーカーごとに処理
-        for i, corner in enumerate(corners):
-            id = ids[i][0]
+        for i, corner in enumerate(corners_list):
+            id = id_list[i]
             this_corner = np.squeeze(corner)
 
             # 姿勢認識
@@ -136,19 +164,19 @@ for current_frame in tqdm.tqdm(range(1, total_frame + 1)):
             markers_tmp.append(this_marker)
 
             if IS_DEBUG:
-                print("marker id: " + str(id))
+                print(f"marker id: {id}")
                 for key, val in this_marker.iteritems():
                     print(key + " : " + str(val[0]))
-
+        
+                
         if len(markers_tmp) < 2:    # マーカーが1つのみ検出された場合
             # concatで複数のマーカーDFを結合してマルチカラムを作成しているため、マーカーDFが1つだけの場合
             # 1つのマーカーを複製して無理やりconcatして、複製した方を最後に削除している
             markers_current = pd.concat([markers_tmp[0], markers_tmp[0]], axis=1, keys=[
-                                        ids[0][0], DELETE_KEY])  # 後で消す方のkey=DELETE_KEY
-
+                                        id_list[0], DELETE_KEY])  # 後で消す方のkey=DELETE_KEY
         else:
             markers_current = pd.concat(
-                markers_tmp, axis=1, keys=np.squeeze(ids))
+                markers_tmp, axis=1, keys=id_list)
 
         IS_DEBUG and print(markers_current)
         markers_hist = pd.concat(
@@ -164,14 +192,14 @@ if DELETE_KEY in markers_hist.columns:  # 不要な行を削除
     markers_hist = markers_hist.drop(DELETE_KEY, axis=1)
 
 # 欠損値をスプライン補完
-print("Complementing missing values...")
-for col in tqdm.tqdm(markers_hist.columns):
-    if (total_frame - markers_hist[col].isnull().sum()) >= 4:
-        markers_hist[col] = markers_hist[col].interpolate(
-            method="spline", order=2, limit_direction="both")
-        if (col[1] == "cpx" or col[1] == "cpy") and ~np.isnan(markers_hist[col][0]):
-            markers_hist[col] = markers_hist[col].astype(int)
+if IS_COMPLEMENT_MISSING_VALUES:
+    print("Complementing missing values...")
+    for col in tqdm.tqdm(markers_hist.columns):
+        if (total_frame - markers_hist[col].isnull().sum()) >= 4:
+            markers_hist[col] = markers_hist[col].interpolate(
+                method="spline", order=2, limit_direction="both")
+            if (col[1] == "cpx" or col[1] == "cpy") and ~np.isnan(markers_hist[col][0]):
+                markers_hist[col] = markers_hist[col].astype(int)
 
-markers_hist
 markers_hist.to_csv(output_path)
 print("out put parsed markers '" + str(output_path) + "'")
